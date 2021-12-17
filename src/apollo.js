3bb8868d06fd409bfa98f25970a393d480cbde3a
justin-dongwook-jung <dongwook.justin.jung@gmail.com>
2021-12-17 23:55:51 +0900

    assignment 10

--- src/apollo.js
+++ src/apollo.js
@@ -1,3 +1,6 @@
 import { makeVar } from "@apollo/client";
 
 export const isLoggedInVar = makeVar(false)
+
+
+
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,21 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: blue;
+  font-family: --apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
+
+const Container = styled.div`
+  background-color: tomato;
+`;
+
 const Login = () => {
-    return (
-      <div>
-        <h1>Login</h1>
-        <button>Log in now!</button>
-      </div>
-    );
-  };
-  
-  export default Login;
-  
+  return (
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  );
+};
+
+export default Login;
